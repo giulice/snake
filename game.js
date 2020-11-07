f78d53116ab015fddcb6afc91471a32d04dd003c
giulice <giulicetto@gmail.com>
2020-11-07 11:11:59 -0300

    Pause game

--- game.js
+++ game.js
@@ -7,7 +7,9 @@ var canvas = null,
     KEY_UP = 38,
     KEY_RIGHT = 39,
     KEY_DOWN = 40,
-    dir = 0;
+    KEY_ENTER = 13,
+    dir = 0,
+    pause = true;
 
     window.requestAnimationFrame = (function (){
         return window.requestAnimationFrame || 
@@ -19,62 +21,75 @@ var canvas = null,
     }());
 
 function paint(ctx) {
+    // Clean canvas
     ctx.fillStyle = '#1E8449';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     
+    // Draw square
     ctx.fillStyle = '#0f0';
     ctx.fillRect(x, y, 10, 10);
 
-    ctx.fillText('Last Press: ' + lastPress, 0, 20);
+    // Debug last key pressed
+    ctx.fillStyle = '#fff';
+    //ctx.fillText('Last Press: ' + lastPress, 0, 20);
+
+    // Draw pause
+if (pause) {
+    ctx.textAlign = 'center';
+    ctx.fillText('PAUSE', 150, 75);
+    ctx.textAlign = 'left';
+    }
 }
 
 function act(){
-    x += 2;
-    if (x > canvas.width) {
-        x = 0;
+    if (!pause) {
+        // Change Direction
+        if (lastPress == KEY_UP) {
+            dir = 0;
+            }
+            if (lastPress == KEY_RIGHT) {
+            dir = 1;
+            }
+            if (lastPress == KEY_DOWN) {
+            dir = 2;
+            }
+            if (lastPress == KEY_LEFT) {
+            dir = 3;
+            }
+
+        // Move Rect
+        if (dir == 0) {
+            y -= 10;
+            }
+            if (dir == 1) {
+            x += 10;
+            }
+            if (dir == 2) {
+            y += 10;
+            }
+            if (dir == 3) {
+            x -= 10;
+            }
+
+        // Out Screen
+        if (x > canvas.width) {
+            x = 0;
+            }
+            if (y > canvas.height) {
+            y = 0;
+            }
+            if (x < 0) {
+            x = canvas.width;
+            }
+            if (y < 0) {
+            y = canvas.height;
+            }
     }
-    
-    // Change Direction
-    if (lastPress == KEY_UP) {
-        dir = 0;
-        }
-        if (lastPress == KEY_RIGHT) {
-        dir = 1;
-        }
-        if (lastPress == KEY_DOWN) {
-        dir = 2;
-        }
-        if (lastPress == KEY_LEFT) {
-        dir = 3;
-        }
-
-    // Move Rect
-    if (dir == 0) {
-        y -= 10;
-        }
-        if (dir == 1) {
-        x += 10;
-        }
-        if (dir == 2) {
-        y += 10;
-        }
-        if (dir == 3) {
-        x -= 10;
-        }
-
-    // Out Screen
-    if (x > canvas.width) {
-        x = 0;
-        }
-        if (y > canvas.height) {
-        y = 0;
-        }
-        if (x < 0) {
-        x = canvas.width;
-        }
-        if (y < 0) {
-        y = canvas.height;
-        }
+    // Pause/Unpause
+    if (lastPress == KEY_ENTER) {
+        pause = !pause;
+        lastPress = null;
+    }
 }
 
 function run() {
