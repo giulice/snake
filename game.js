6a706862f01fe4aeb79765feb9490df0c42512de
giulice <giulicetto@gmail.com>
2020-11-07 10:31:54 -0300

    Keydown

--- game.js
+++ game.js
@@ -1,15 +1,48 @@
 var canvas = null,
-    ctx = null;
+    ctx = null,
+    x = 50,
+    y = 50,
+    lastPress = null;
+
+    window.requestAnimationFrame = (function (){
+        return window.requestAnimationFrame || 
+            window.mozRequestAnimationFrame ||
+            window.webkitRequestAnimationFrame ||
+            function(callback) {
+                window.setTimeout(callback,17);
+            };
+    }());
 
 function paint(ctx) {
+    ctx.fillStyle = '#1E8449';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
     ctx.fillStyle = '#0f0';
-    ctx.fillRect(50, 50, 100, 60);
+    ctx.fillRect(x, y, 10, 10);
+
+    ctx.fillText('Last Press: ' + lastPress, 0, 20);
 }
+
+function act(){
+    x += 2;
+    if (x > canvas.width) {
+        x = 0;
+    }
+    }
+
+function run() {
+    window.requestAnimationFrame(run);
+    act();
+    paint(ctx);
+    }
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    paint(ctx);
+    run();
 }
 
 window.addEventListener('load', init, false);
+document.addEventListener('keydown', function (evt) {
+    lastPress = evt.which;
+    }, false);
